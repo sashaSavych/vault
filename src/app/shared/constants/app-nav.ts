1a1c0a0a1ea1eea98f31ca2b3932f7d1e66981cf
Oleksandr Savych <respect2203@gmail.com>
2026-06-15 10:02:49 +0300

    Improve UI

--- src/app/shared/constants/app-nav.ts
+++ src/app/shared/constants/app-nav.ts
@@ -5,14 +5,14 @@ export interface NavPage {
 }
 
 export const APP_NAV_PAGES: NavPage[] = [
-  { path: '/outcomes', label: 'Outcomes', icon: 'pi-minus-circle' },
+  { path: '/accounts', label: 'Accounts', icon: 'pi-wallet' },
   { path: '/incomes', label: 'Incomes', icon: 'pi-plus-circle' },
+  { path: '/outcomes', label: 'Outcomes', icon: 'pi-minus-circle' },
   { path: '/transfers', label: 'Transfers', icon: 'pi-arrow-right-arrow-left' },
   { path: '/more', label: 'More', icon: 'pi-ellipsis-h' },
 ];
 
 export const MORE_NAV_PAGES: NavPage[] = [
-  { path: '/accounts', label: 'Accounts', icon: 'pi-wallet' },
   { path: '/categories', label: 'Categories', icon: 'pi-tags' },
   { path: '/debts', label: 'Debts', icon: 'pi-book' },
   { path: '/reports', label: 'Reports', icon: 'pi-chart-bar' },
